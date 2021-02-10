2cbca86a85d41bd8b64993ea510124239a608900
RG-Mac <ramong5406@gmail.com>
2021-02-09 18:49:48 -0600

    created files for Navbar components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar/index';
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <h1>PIZZA SITE COMING SOON!</h1>
     </div>
   )
