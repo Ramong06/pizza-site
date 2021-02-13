84a8b943c4ed4781b8d104a3a063854476e1d4c3
RG-Mac <ramong5406@gmail.com>
2021-02-13 14:49:00 -0600

    added and styled Hero components

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Navbar from '../Navbar';
-import { HeroContainer } from './HeroElements';
+import { HeroContainer, HeroContent, HeroBtn, HeroH1, HeroP, HeroItems } from './HeroElements';
 
 const Hero = () => {
     return (
         <HeroContainer>
             <Navbar />
+            <HeroContent>
+                <HeroItems>
+                    <HeroH1>Greatest Pizza Ever</HeroH1>
+                    <HeroP>Ready in 60 Seconds</HeroP>
+                    <HeroBtn>Place Order</HeroBtn>
+                </HeroItems>
+            </HeroContent>
         </HeroContainer>
     )
 }
